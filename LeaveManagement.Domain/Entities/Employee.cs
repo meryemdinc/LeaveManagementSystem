c06886da8eb9f8538b7db5210cb22c06b2cea83b
meryemdinc <meryemdinc45@gmail.com>
2025-11-25 17:11:30 +0300

    feat: add Employee entity with basic properties and navigation to LeaveRequests

---
+++ LeaveManagement.Domain/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Domain.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; } // Şifreler asla düz metin tutulmaz
+        public string Role { get; set; } // "Admin" veya "Employee"
+
+        // Navigation Property
+        // Bir çalışanın birden fazla izin talebi olabilir.
+        public ICollection<LeaveRequest> LeaveRequests { get; set; }
+    }
+}
