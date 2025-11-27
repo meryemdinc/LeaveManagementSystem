32311b849c9428bc3b5f238d0c46e0e06086e24f
meryemdinc <meryemdinc45@gmail.com>
2025-11-27 22:14:46 +0300

    feat: add LeaveStatus enum with pending, approved, rejected and cancelled states

---
+++ LeaveManagement.Domain/Enums/LeaveStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace LeaveManagement.Domain.Enums
+{
+    public enum LeaveStatus
+    {
+        Pending = 1,  // Beklemede
+        Approved = 2, // Onaylandı
+        Rejected = 3, // Reddedildi
+        Cancelled = 4 // İptal Edildi
+    }
+}
