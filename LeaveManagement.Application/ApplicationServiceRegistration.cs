d0cd8ab0ea9d4704a99ba79640332a63bbf5990e
meryemdinc <meryemdinc45@gmail.com>
2025-11-30 23:08:31 +0300

    feat: add JWT-based AuthService and configure validators and AutoMapper in Application layer

---
+++ LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using LeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+
+namespace LeaveManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // AutoMapper ayarları
+            services.AddAutoMapper(cfg =>
+            {
+                // 1. BU SATIR ÇOK ÖNEMLİ:
+                // AutoMapper'ın "GetTotal", "MaxFloat" gibi metodları otomatik bulup 
+                // eşleştirmeye çalışmasını engelliyoruz. Sadece Property'leri (Ad, Soyad) eşleştirir.
+                cfg.ShouldMapMethod = (m) => false;
+
+                // 2. Profilimizi elle ekliyoruz
+                cfg.AddProfile<MappingProfile>();
+            });
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
