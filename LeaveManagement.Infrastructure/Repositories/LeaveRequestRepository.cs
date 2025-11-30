2feee5c414ba3c6521732afc6e75f2f99993a9a1
meryemdinc <meryemdinc45@gmail.com>
2025-11-30 23:37:27 +0300

    feat: add LeaveRequestRepository with methods to fetch leave requests including employee details

---
+++ LeaveManagement.Infrastructure/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,38 @@
+﻿using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Domain.Entities;
+using LeaveManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Infrastructure.Repositories
+{
+    public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+    {
+        public LeaveRequestRepository(LeaveManagementDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            var leaveRequests = await _context.LeaveRequests
+                .Include(q => q.Employee) // Employee verisini de getir (Eager Loading)
+                .ToListAsync();
+
+            return leaveRequests;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(int id)
+        {
+            var leaveRequests = await _context.LeaveRequests
+                .Include(q => q.Employee)
+                .Where(q => q.Id == id)
+                .ToListAsync();
+
+            return leaveRequests;
+        }
+    }
+}
