e4c5dba3becefbf9927595dcff204e548359c147
meryemdinc <meryemdinc45@gmail.com>
2025-11-30 22:54:46 +0300

    feat: add generic repository interface with basic CRUD operations

---
+++ LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//repository interface,veritabanına nasıl erişileceğinin kurallarını belirler,
+//temel CRUD işlemlerini tanımlar.
+namespace LeaveManagement.Application.Contracts.Persistence
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
