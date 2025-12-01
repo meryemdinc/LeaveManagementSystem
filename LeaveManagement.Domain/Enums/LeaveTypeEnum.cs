74f9b51e4a11c827ac7534cf733aea3c2e581255
meryemdinc <meryemdinc45@gmail.com>
2025-12-01 20:16:54 +0300

    feat: add LeaveTypeEnum with Annual, Sick, and Unpaid values

---
+++ LeaveManagement.Domain/Enums/LeaveTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace LeaveManagement.Domain.Enums
+{
+    public enum LeaveTypeEnum
+    {
+        Annual = 1,    // Yıllık İzin (Bakiyeden düşer)
+        Sick = 2,      // Hastalık İzni
+        Unpaid = 3     // Ücretsiz İzin
+        // Diğerleri veritabanına eklenebilir ama kod tarafında özel logic gerektirenler bunlar.
+    }
+}
