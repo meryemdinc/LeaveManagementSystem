8f07067a860b48d3e14b9b0a42deb261264e6881
meryemdinc <meryemdinc45@gmail.com>
2025-12-01 20:01:41 +0300

    feat: add ILeaveRequestService interface with CreateLeaveRequest and ApproveRequest methods

---
+++ LeaveManagement.Application/Contracts/Services/ILeaveRequestService.cs
@@ -0,0 +1,14 @@
+﻿using LeaveManagement.Application.Common.Wrappers;
+using LeaveManagement.Application.DTOs.LeaveRequest;
+
+//UnitOfWork kullanarak veritabanı işlemlerini yönetiyor
+
+
+namespace LeaveManagement.Application.Contracts.Services
+{
+    public interface ILeaveRequestService
+    {
+        Task<Response<int>> CreateLeaveRequest(CreateLeaveRequestDto dto);
+        Task<Response<bool>> ApproveRequest(int id, bool approved);
+    }
+}
