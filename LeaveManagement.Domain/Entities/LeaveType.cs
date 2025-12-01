5372aaf160faf465d975d3fb0e34ca6939eedac8
meryemdinc <meryemdinc45@gmail.com>
2025-12-01 20:15:41 +0300

    feat: add LeaveType entity with Name and DefaultDays fields

---
+++ LeaveManagement.Domain/Entities/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿using LeaveManagement.Domain.Common;
+
+
+namespace LeaveManagement.Domain.Entities
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
