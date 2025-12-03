83cb977d6ae3df4dabfa0304c7c12f2a70948d50
meryemdinc <meryemdinc45@gmail.com>
2025-12-03 10:42:59 +0300

    feat(Identity): Define IAuthService contract

---
+++ LeaveManagement.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using LeaveManagement.Application.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(LoginDto request);
+    }
+}
