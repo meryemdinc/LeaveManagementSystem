16dfc2ebfcb5bdc2bd4a65ae028b53eeb5b8d31b
meryemdinc <meryemdinc45@gmail.com>
2025-12-03 10:53:52 +0300

    Feat: Define the DTO for creating a new leave request, including required dates, the associated leave type ID, and the reason for the request.

---
+++ LeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using LeaveManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class CreateLeaveRequestDto
+    {
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+       
+        public int LeaveTypeId { get; set; }
+        [MaxLength(300)]
+        public string Reason { get; set; }
+
+        public int EmployeeId { get; set; } // Şimdilik manuel, ileride Token'dan alacağız
+    }
+}
