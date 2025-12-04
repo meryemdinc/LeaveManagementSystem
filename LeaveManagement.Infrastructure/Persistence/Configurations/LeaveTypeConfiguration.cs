c2c79c342e6736e0957dafafe03b5add318485f9
meryemdinc <meryemdinc45@gmail.com>
2025-12-04 11:52:09 +0300

    feat: Configure LeaveType entity and seed initial leave types with EF Core.

---
+++ LeaveManagement.Infrastructure/Persistence/Configurations/LeaveTypeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using LeaveManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace LeaveManagement.Infrastructure.Persistence.Configurations
+{
+    public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+    {
+        public void Configure(EntityTypeBuilder<LeaveType> builder)
+        {
+            // Tablo Ayarları
+            builder.HasKey(q => q.Id); // Primary Key
+            builder.Property(q => q.Name).IsRequired().HasMaxLength(50); // Zorunlu ve max 50 karakter
+
+            // --- SEED DATA (Başlangıç Verileri) ---
+            // Migration çalışınca bu veriler otomatik eklenecek!
+            builder.HasData(
+                new LeaveType
+                {
+                    Id = 1,
+                    Name = "Yıllık İzin",
+                    DefaultDays = 14,
+                    CreatedDate = DateTime.UtcNow
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    Name = "Mazeret İzni",
+                    DefaultDays = 5,
+                    CreatedDate = DateTime.UtcNow
+                },
+                new LeaveType
+                {
+                    Id = 3,
+                    Name = "Hastalık İzni",
+                    DefaultDays = 10,
+                    CreatedDate = DateTime.UtcNow
+                }
+            );
+        }
+    }
+}
