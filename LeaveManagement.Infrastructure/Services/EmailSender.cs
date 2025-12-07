e7cae80ba6f736a6dd94771f566d64d602d1c235
meryemdinc <meryemdinc45@gmail.com>
2025-12-07 22:33:15 +0300

    feat: implement EmailSender service with SMTP-based email sending

---
+++ LeaveManagement.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,61 @@
+﻿using LeaveManagement.Application.Contracts.Infrastructure;
+using LeaveManagement.Application.DTOs.Email;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<bool> SendEmail(EmailRequest emailRequest)
+        {
+            try
+            {
+                // Ayarları appsettings.json'dan oku
+                var host = _configuration["EmailSettings:Host"];
+                var port = int.Parse(_configuration["EmailSettings:Port"]);
+                var fromEmail = _configuration["EmailSettings:From"];
+                var username = _configuration["EmailSettings:Username"];
+                var password = _configuration["EmailSettings:Password"];
+
+                // SMTP İstemcisini Hazırla
+                using (var client = new SmtpClient(host, port))
+                {
+                    client.EnableSsl = true; // Güvenli bağlantı
+                    client.Credentials = new NetworkCredential(username, password);
+
+                    var mailMessage = new MailMessage
+                    {
+                        From = new MailAddress(fromEmail),
+                        Subject = emailRequest.Subject,
+                        Body = emailRequest.Body,
+                        IsBodyHtml = true // HTML formatında mail atabiliriz
+                    };
+
+                    mailMessage.To.Add(emailRequest.To);
+
+                    // Gönder!
+                    await client.SendMailAsync(mailMessage);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Hata olursa loglayabiliriz ama şimdilik false dönelim
+                // Console.WriteLine(ex.Message); 
+                return false;
+            }
+        }
+    }
+}
