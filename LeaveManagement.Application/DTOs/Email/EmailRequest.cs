878778df3bd8cd45ee7d425b5ce2865dc2dae44b
meryemdinc <meryemdinc45@gmail.com>
2025-12-07 22:32:13 +0300

    feat: add EmailRequest DTO for email sending operations

---
+++ LeaveManagement.Application/DTOs/Email/EmailRequest.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace LeaveManagement.Application.DTOs.Email
+{
+    public class EmailRequest
+    {
+        public string To { get; set; }      // Kime
+        public string Subject { get; set; } // Konu
+        public string Body { get; set; }    // İçerik
+    }
+}
